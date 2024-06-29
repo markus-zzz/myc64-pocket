edf92da8c043e555e3f9d1608b5c4a90f071ddf1
Markus Lavin <markus.lavin@gmail.com>
2024-06-29 07:55:48 +0200

    Use GHDL to convert VHDL T65 to Verilog

--- src/fpga/build-sim.sh
+++ src/fpga/build-sim.sh
@@ -15,10 +15,19 @@ pushd core/my1541-rtl
 python3 my1541.py
 popd
 
+# Convert VHDL T65 to Verilog
+rm -rf build
+mkdir build
+ghdl -i --std=08 --work=t65 --workdir=build -Pbuild core/myc64-rtl/T65/*.vhd
+ghdl -m -fsynopsys --std=08 --work=t65 --workdir=build T65
+ghdl synth -fsynopsys --std=08 --work=t65 --workdir=build -Pbuild --out=verilog T65 > T65.v
+# XXX: For some reason ghdl gives us a duplicate signal declaration and Verilator does not like that - simply delete the duplicate line with sed
+sed -i -e '6640d' T65.v
+
 OBJ_DIR=obj_dir
 rm -rf $OBJ_DIR
 
-/home/markus/work/install/bin/verilator --trace-fst -cc +1364-2005ext+v --top-module core_top core/spram.v core/sprom.v core/core_top.v core/core_bridge_cmd.v apf/common.v core/myc64-rtl/myc64.v core/my1541-rtl/my1541.v core/myc64-rtl/cpu-tv65/rtl/*.v -Icore/myc64-rtl/cpu-tv65/rtl/ core/picorv32.v -Wno-fatal \
+/home/markus/work/install/bin/verilator --trace-fst -cc +1364-2005ext+v --top-module core_top core/spram.v core/sprom.v core/core_top.v core/core_bridge_cmd.v apf/common.v core/myc64-rtl/myc64.v core/my1541-rtl/my1541.v T65.v core/picorv32.v -Wno-fatal \
 +define+__VERILATOR__=1 -CFLAGS -O3
 
 VERILATOR_ROOT=/home/markus/work/install/share/verilator
